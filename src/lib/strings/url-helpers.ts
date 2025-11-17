m-doescode/antlers-social

feat: setting to override share link url

src/lib/strings/url-helpers.ts
@@ -6,6 +6,7 @@ import {BSKY_SERVICE} from '#/lib/constants'
 import {isInvalidHandle} from '#/lib/strings/handles'
 import {startUriToStarterPackUri} from '#/lib/strings/starter-pack'
 import {logger} from '#/logger'
+import {getCustomShareLink} from '#/state/preferences/custom-share-link'
 
 export const BSKY_APP_HOST = 'https://deer.social'
 const BSKY_TRUSTED_HOSTS = [
@@ -80,7 +81,7 @@ export function toShortUrl(url: string): string {
 
 export function toShareUrl(url: string): string {
   if (!url.startsWith('https')) {
-    const urlp = new URL('https://deer.social')
+    const urlp = new URL(getCustomShareLink() ?? 'https://deer.social')
     urlp.pathname = url
     url = urlp.toString()
   }

src/state/persisted/schema.ts
@@ -144,6 +144,7 @@ const schema = z.object({
   // antlers
   renderFullMastodonPostText: z.boolean().optional(),
   showExternalShareButtons: z.boolean().optional(),
+  customShareLink: z.string(),
 
   /** @deprecated */
   mutedThreads: z.array(z.string()),
@@ -221,6 +222,7 @@ export const defaults: Schema = {
   // antlers
   renderFullMastodonPostText: false,
   showExternalShareButtons: false,
+  customShareLink: 'https://deer.social/',
 }
 
 export function tryParse(rawData: string): Schema | undefined {

pyrox0/catsky-social

src/lib/strings/url-helpers.ts
@@ -6,6 +6,7 @@ import {BSKY_SERVICE} from '#/lib/constants'
 import {isInvalidHandle} from '#/lib/strings/handles'
 import {startUriToStarterPackUri} from '#/lib/strings/starter-pack'
 import {logger} from '#/logger'
+import {getCustomShareLink} from '#/state/preferences/custom-share-link'
 
 export const BSKY_APP_HOST = 'https://bsky.app'
 const BSKY_TRUSTED_HOSTS = [
@@ -79,7 +80,7 @@ export function toShortUrl(url: string): string {
 
 export function toShareUrl(url: string): string {
   if (!url.startsWith('https')) {
-    const urlp = new URL('https://catsky.social')
+    const urlp = new URL(getCustomShareLink() ?? 'https://catsky.social')
     urlp.pathname = url
     url = urlp.toString()
   }

src/state/persisted/schema.ts
@@ -131,6 +131,7 @@ const schema = z.object({
   unfollowConfirm: z.boolean().optional(),
 
   showExternalShareButtons: z.boolean().optional(),
+  customShareLink: z.string(),
 
   /** @deprecated */
   mutedThreads: z.array(z.string()),
@@ -190,6 +191,7 @@ export const defaults: Schema = {
   directFetchRecords: false,
   unfollowConfirm: false,
   showExternalShareButtons: false,
+  customShareLink: 'https://catsky.social/',
 }
 
 export function tryParse(rawData: string): Schema | undefined {